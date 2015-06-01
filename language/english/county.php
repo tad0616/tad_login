ad251c28a729c81a9177784942f975ef1bddb944
mambax7 <mambax7@gmail.com>
2015-06-01 00:40:18 -0400

    Adding English translation files

---
+++ language/english/county.php
@@ -0,0 +1,27 @@
+<?php
+
+define('_FACEBOOK', 'Facebook');
+define('_GOOGLE', 'Google');
+define('_GOOGLE_V2', 'Google OAuth2.0');
+define('_TWITTER', 'Twitter');
+define('_YAHOO', 'Yahoo');
+define('_MYID', 'MyID');
+define('_TN', 'Tainan City Department of Education OpenID');
+define('_CYC', 'Chiayi County Education Office OpenID');
+define('_YLC', 'Yunlin County Education Office OpenID');
+define('_HCC', 'Hsinchu County Education Office OpenID');
+define('_MLC', 'Miaoli County Education Office OpenID');
+define('_CHC', 'Changhua County Education Office OpenID');
+define('_CY', 'Chiayi City Education Department OpenID');
+define('_TC', 'Taichung City Bureau of Education OpenID');
+define('_PTC', 'Pingtung County Education Office OpenID');
+define('_PHC', 'Penghu County Education Office OpenID');
+define('_KL', 'Keelung and Education OpenID');
+define('_HLC', 'Hualien County Education Office OpenID');
+define('_NTPC', 'New Taipei City Bureau of Education OpenID');
+define('_ILC', 'Ilan County Education Office OpenID');
+define('_NTCT', 'Nantou County Education Office OpenID');
+define('_HC', 'Hsinchu Education Office OpenID');
+define('_KH', 'Kaohsiung City Bureau of Education OpenID');
+define('_TYC', 'Taoyuan County Education Office OpenID');
+define('_KM', 'Kinmen County Education Office OpenID');
