c5c86d2f632ce585d18b8ec7a4e1b8eda8041398
Tad <tad0616@gmail.com>
2019-01-03 09:49:55 +0800

    台北市登入位址改回舊的

--- class/edu/auth.php
+++ class/edu/auth.php
@@ -3,11 +3,9 @@
 require __DIR__ . '/vendor/autoload.php';
 require_once __DIR__ . '/config.php';
 use Jumbojett\OpenIDConnectClient;
+$oidc = new OpenIDConnectClient($provideruri, $clientid, $clientsecret);
 
-$oidc = new OpenIDConnectClient($provideruri,
-    $clientid, $clientsecret);
-
-$oidc->setResponseTypes($resonposetype);
+$oidc->setResponseTypes($responsetype);
 $oidc->setRedirectURL($redirecturi);
 //$oidc->setState(generateRandomString());
 //$oidc->setNonce(generateRandomString());
