dad0ea0d6157deb7fe6c2722f8260f66b94b26d1
Tad <tad0616@gmail.com>
2019-04-30 12:43:00 +0800

    加語系

--- language/english/main.php
+++ language/english/main.php
@@ -1,4 +1,5 @@
 <?php
+xoops_loadLanguage('main', 'tadtools');
 include_once 'county.php';
 // Need to add modules Languages
 define('_TAD_NEED_TADTOOLS', 'This module needs TadTools module. You can download TadTools from <a href="http://campus-xoops.tn.edu.tw/modules/tad_modules/index.php?module_sn=1" target="_blank">XOOPS EasyGO</a>.');
