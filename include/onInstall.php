0b075782741b01971228ece30cba86e9dbab8f87
mambax7 <mambax7@gmail.com>
2019-05-12 01:04:57 -0400

    Unnecessary double quotes
    Signed-off-by: mambax7 <mambax7@gmail.com>

--- include/onInstall.php
+++ include/onInstall.php
@@ -8,7 +8,7 @@ if (!class_exists('XoopsModules\Tadtools\Utility')) {
 function xoops_module_install_tad_login(&$module)
 {
 
-    Utility::mk_dir(XOOPS_ROOT_PATH . "/uploads/tad_login");
+    Utility::mk_dir(XOOPS_ROOT_PATH . '/uploads/tad_login');
 
     return true;
 }
--- include/onUninstall.php
+++ include/onUninstall.php
@@ -5,9 +5,9 @@ use XoopsModules\Tadtools\Utility;
 function xoops_module_uninstall_tad_login(&$module)
 {
     global $xoopsDB;
-    $date = date("Ymd");
+    $date = date('Ymd');
 
-    rename(XOOPS_ROOT_PATH . "/uploads/tad_login", XOOPS_ROOT_PATH . "/uploads/tad_login_bak_{$date}");
+    rename(XOOPS_ROOT_PATH . '/uploads/tad_login', XOOPS_ROOT_PATH . "/uploads/tad_login_bak_{$date}");
 
     return true;
 }
