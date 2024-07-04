0b12da620b4d6091c69bd77e323dd8638a20fea3
tad0616 <tad0616@gmail.com>
2024-07-04 09:07:51 +0800

    支援XOOPS 2.5.11

--- admin/header.php
+++ admin/header.php
@@ -30,7 +30,7 @@ xoops_cp_header();
 // Define Stylesheet and JScript
 $xoTheme->addStylesheet(XOOPS_URL . '/modules/tadtools/css/iconize.css');
 $xoTheme->addStylesheet(XOOPS_URL . '/modules/tadtools/css/font-awesome/css/font-awesome.css');
-$xoTheme->addStylesheet(XOOPS_URL . "/modules/tadtools/css/xoops_adm{$_SEESION['bootstrap']}.css");
+$xoTheme->addStylesheet(XOOPS_URL . "/modules/tadtools/css/xoops_adm{$_SESSION['bootstrap']}.css");
 $xoTheme->addStylesheet(XOOPS_URL . '/modules/' . $xoopsModule->getVar('dirname') . '/css/admin.css');
 //$xoTheme->addScript("browse.php?Frameworks/jquery/jquery.js");
 //$xoTheme->addScript("browse.php?modules/" . $xoopsModule->getVar("dirname") . "/js/admin.js");
