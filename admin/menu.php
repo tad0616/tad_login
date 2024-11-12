4e2d46880bcb94df6f8232e9f4ffc51950def0c5
tad0616 <tad0616@gmail.com>
2024-11-12 16:24:11 +0800

    簡化後台選單陣列，修改釋出日期

--- admin/menu.php
+++ admin/menu.php
@@ -1,57 +1,43 @@
 <?php
-$adminmenu = [];
-$icon_dir = '2.6' === mb_substr(XOOPS_VERSION, 6, 3) ? '' : 'images/';
-
-$i = 1;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_HOME;
-$adminmenu[$i]['link'] = 'admin/index.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_HOME_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/home.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU2;
-$adminmenu[$i]['link'] = 'admin/main.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU2;
-$adminmenu[$i]['icon'] = 'images/admin/main.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU4;
-$adminmenu[$i]['link'] = 'admin/oidc.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU4;
-$adminmenu[$i]['icon'] = 'images/admin/oidc.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU5;
-$adminmenu[$i]['link'] = 'admin/ps_tool.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU5;
-$adminmenu[$i]['icon'] = 'images/admin/ps_tool.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU7;
-$adminmenu[$i]['link'] = 'admin/change_uid.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU7;
-$adminmenu[$i]['icon'] = 'images/admin/change_uid.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU1;
-$adminmenu[$i]['link'] = 'admin/fb.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU1;
-$adminmenu[$i]['icon'] = 'images/facebook.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU3;
-$adminmenu[$i]['link'] = 'admin/google.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU3;
-$adminmenu[$i]['icon'] = 'images/google.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TADLOGIN_ADMENU6;
-$adminmenu[$i]['link'] = 'admin/line.php';
-$adminmenu[$i]['desc'] = _MI_TADLOGIN_ADMENU6;
-$adminmenu[$i]['icon'] = 'images/line.png';
-
-$i++;
-$adminmenu[$i]['title'] = _MI_TAD_ADMIN_ABOUT;
-$adminmenu[$i]['link'] = 'admin/about.php';
-$adminmenu[$i]['desc'] = _MI_TAD_ADMIN_ABOUT_DESC;
-$adminmenu[$i]['icon'] = 'images/admin/about.png';
+$adminmenu = [
+    [
+        'title' => _MI_TADLOGIN_ADMENU2,
+        'link' => 'admin/main.php',
+        'icon' => 'images/admin/main.png',
+    ],
+    [
+        'title' => _MI_TADLOGIN_ADMENU4,
+        'link' => 'admin/oidc.php',
+        'icon' => 'images/admin/oidc.png',
+    ],
+    [
+        'title' => _MI_TADLOGIN_ADMENU5,
+        'link' => 'admin/ps_tool.php',
+        'icon' => 'images/admin/ps_tool.png',
+    ],
+    [
+        'title' => _MI_TADLOGIN_ADMENU7,
+        'link' => 'admin/change_uid.php',
+        'icon' => 'images/admin/change_uid.png',
+    ],
+    [
+        'title' => _MI_TADLOGIN_ADMENU1,
+        'link' => 'admin/fb.php',
+        'icon' => 'images/facebook.png',
+    ],
+    [
+        'title' => _MI_TADLOGIN_ADMENU3,
+        'link' => 'admin/google.php',
+        'icon' => 'images/google.png',
+    ],
+    [
+        'title' => _MI_TADLOGIN_ADMENU6,
+        'link' => 'admin/line.php',
+        'icon' => 'images/line.png',
+    ],
+    [
+        'title' => _MI_TAD_ADMIN_ABOUT,
+        'link' => 'admin/about.php',
+        'icon' => 'images/admin/about.png',
+    ],
+];
